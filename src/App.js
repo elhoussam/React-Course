45b25cb799957e14ec62c393ff790500717e9614
ElHoussam <houssam.glsd@gmail.com>
2024-01-04 14:19:44 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { monsters: [], searchField: "" };
+  }
+  componentDidMount() {
+    this.fetching();
+  }
+  fetching() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        return res.json();
+      })
+      .then((userss) => {
+        // console.log(userss);
+        this.setState(
+          () => {
+            return { monsters: userss };
+          },
+          () => console.log(this.state)
+        );
+      });
+  }
+  render() {
+    let filteredArr = this.state.monsters.filter((x) => {
+      return x.name.includes(this.state.searchField);
+    });
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Name your monster"
+          onChange={(event) => this._renderMonster(event.target.value)}
+        />
+        <main className="box">
+          {filteredArr.map((m) => {
+            return <h1 key={m.id}> {m.name}</h1>;
+          })}
+        </main>
+      </div>
+    );
+  }
+  _renderMonster(filter = "") {
+    this.setState({ searchField: filter });
+  }
 }
 
 export default App;
