7bbed4a2feeed7ca7408925ddf79b1711d7c6e13
ElHoussam <houssam.glsd@gmail.com>
2024-01-10 20:26:19 +0100

    refact: card list component into functional component

--- src/component/card-list/card-list.component.jsx
+++ src/component/card-list/card-list.component.jsx
@@ -1,18 +1,17 @@
-import { Component } from "react";
+// import { Component } from "react";
 import Card from "../card/card.component.jsx";
 
 import "./card.styles.css";
-class CardList extends Component {
-  render() {
-    let { robots } = this.props;
-    // console.log("card list", robots);
-    return (
-      <div className="card-list">
-        {robots.map((m) => {
-          return <Card robot={m} key={m.id} />;
-        })}
-      </div>
-    );
-  }
-}
+
+const CardList = (props) => {
+  let { robots } = props;
+  // console.log("card list", robots);
+  return (
+    <div className="card-list">
+      {robots.map((m) => {
+        return <Card robot={m} key={m.id} />;
+      })}
+    </div>
+  );
+};
 export default CardList;
